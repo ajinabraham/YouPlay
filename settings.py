65dcdc1bade884134c45a463d38863b4adf3fb6c
ajinabraham <ajin25@gmail.com>
2016-05-04 09:21:38 +0530

    alpha

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+dirname = os.path.dirname(__file__)
+
+STATIC_PATH = os.path.join(dirname, 'static')
+TEMPLATE_PATH = os.path.join(dirname, 'templates')
+
+YOUTUBE_DL='youtube-dl'
+DOWNLOAD_DIR = os
